a38edd71588569013aa30f2e3f7a220b3c5c4161
saan <hansan0529@gmail.com>
2025-06-23 17:34:17 +0900

    svgr webpack 설정 SVG 파일을 컴포넌트로 호출 할 수 있도록 함

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,55 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export",
-  assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "https://hx529.github.io/nextjs-ecommerce/"
-      : "",
+  // TurboPack 설정
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+  // webpack 설정
+  webpack: (config) => {
+    // @ts-expect-error 타입 에러 무시
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              icon: true,
+              replaceAttrValues: {
+                '#000': 'currentColor',
+                '#000000': 'currentColor',
+                '#fff': 'currentColor',
+                '#ffffff': 'currentColor',
+              },
+              typescript: true,
+              ext: 'tsx',
+            },
+          },
+        ],
+      }
+    );
+    fileLoaderRule.exclude = /\.svg$/i;
+    return config;
+  },
 };
 
 export default nextConfig;

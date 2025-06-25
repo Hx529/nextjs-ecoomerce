7adad9d34c7d55680d83b83a2e8ad9a7f94161da
saan <hansan0529@gmail.com>
2025-06-25 13:36:43 +0900

    검색 순위 목록 표기

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,7 +5,7 @@ import HeaderBannerText from "@/components/banner/header-banner-text";
 import Image from "next/image";
 import Link from "next/link";
 
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, Fragment } from "react";
 
 // i 높이
 const HEADER_HEIGHT = 118;
@@ -176,6 +176,34 @@ const categoryMenuData = [
 	},
 ];
 
+// f Search 검색 순위 (고정 데이터)
+const SearchedData = {
+  "SearchedBrands": [
+    "MUSINSA STANDARD",
+    "LEMAIN SECOND",
+    "MUSINSA STANDARD WOMAN",
+    "LIKE THE MOST",
+    "GAKKAI UNIONS",
+    "TRAVEL",
+    "NINEZ",
+    "SIGNATURE",
+    "AGOD",
+    "DIMITRI BLACK",
+  ],
+  "Popular": [
+    "glowny",
+    "jeans",
+    "Baggy jeans",
+    "sweatpants",
+    "Hoodie",
+    "jorts",
+    "shorts",
+    "tank top",
+    "jersey",
+    "Jacket",
+  ]
+};
+
 const Header = () => {
 	const [isVisiable, setIsVisiable] = useState(false); // i 카테고리 팝업 표시
 	const [isSearchOpen, setIsSearchOpen] = useState(false); // i 검색창 표시
@@ -196,133 +224,135 @@ const Header = () => {
 			<header
 				className={`w-full flex flex-col items-center fixed bg-white z-10 h-[${HEADER_HEIGHT}px]`}
 			>
-				<h3 className="sr-only">Header</h3>
-				<div className="max-w-7xl w-full h-[70px] flex justify-between items-center">
-					{/* Search */}
-					<div
-						className="relative rounded-sm bg-search cursor-pointer flex items-center w-2xs h-1/2 gap-2 px-3"
-						onClick={() => setIsSearchOpen(true)}
-					>
-						<SearchIcon title="search icon" className="w-[18px] h-[18px]" />
-						<span className="text-placeholder">Search</span>
-					</div>
-					{/* Logo */}
-					<h1 className="w-[6.25rem] h-full flex items-center justify-center">
-						<Link href="/" className="relative w-full h-full">
-							<Image
-								src="/images/components/header/hxinsa.svg"
-								alt="Brand Logo"
-								className="object-contain"
-								fill
-							/>
-						</Link>
-					</h1>
-					{/* User */}
-					<div>
-						<ul className="flex items-center gap-3">
-							{userMenuData.map((item) => (
-								<li key={item.key}>
-									{item.type === "button" ? (
-										<button
-											className="relative cursor-pointer hover:text-primary flex items-center gap-1"
-											title={item.title}
-										>
+        <section className="max-w-7xl m-auto">
+          <h3 className="sr-only">Header</h3>
+          <div className="h-[70px] flex justify-between items-center">
+            {/* Search */}
+            <div
+              className="relative rounded-sm bg-search cursor-pointer flex items-center w-2xs h-1/2 gap-2 px-3"
+              onClick={() => setIsSearchOpen(true)}
+            >
+              <SearchIcon title="search icon" className="w-[18px] h-[18px]" />
+              <span className="text-placeholder">Search</span>
+            </div>
+            {/* Logo */}
+            <h1 className="w-[6.25rem] h-full flex items-center justify-center">
+              <Link href="/" className="relative w-full h-full">
+                <Image
+                  src="/images/components/header/hxinsa.svg"
+                  alt="Brand Logo"
+                  className="object-contain"
+                  fill
+                />
+              </Link>
+            </h1>
+            {/* User */}
+            <div>
+              <ul className="flex items-center gap-3">
+                {userMenuData.map((item) => (
+                  <li key={item.key}>
+                    {item.type === "button" ? (
+                      <button
+                        className="relative cursor-pointer hover:text-primary flex items-center gap-1"
+                        title={item.title}
+                      >
+                        <item.icon />
+                        <p id="location">{item.label}</p>
+                      </button>
+                    ) : item.type === "link" && item.key === "bag" ? (
+                      <Link
+                      href={item.href!}
+                      className="relative block hover:text-primary"
+                      title={item.title}
+                      >
                       <item.icon />
-											<p id="location">{item.label}</p>
-										</button>
-									) : item.type === "link" && item.key === "bag" ? (
-                    <Link
-                    href={item.href!}
-                    className="relative block hover:text-primary"
-                    title={item.title}
-										>
-                    <item.icon />
-											<span
-												id="shoppingList"
-												className="block absolute bottom-0 right-0 bg-black min-w-[1.0625rem] rounded-full text-white text-xs text-center px-1"
-											>
-												0
-											</span>
-										</Link>
-									) : (
-										<Link
-											href={item.href!}
-											className="relative hover:text-primary"
-											title={item.title}
-										>
-                      <item.icon />
-										</Link>
-									)}
-								</li>
-							))}
-						</ul>
-					</div>
-				</div>
-				{/* Category */}
-				<nav className="max-w-7xl w-full relative h-12">
-					<h3 className="sr-only">Category</h3>
-					<ul className="flex items-center gap-10 max-w-7xl w-full h-full font-bold text-sm relative">
-						<div
-							className="relative h-full flex items-center"
-							onMouseEnter={() => setIsVisiable(true)}
-							onMouseLeave={() => setIsVisiable(false)}
-							ref={categoryRef}
-						>
-							<li className="h-full flex items-center custom-expanded-weight custom-before-division">
-								<Link
-									href="/"
-									className="relative hover:text-primary flex items-center gap-3"
-								>
-                  <HamburgerMenuIcon title="hamburger menu" className="w-[18px] h-[16px]" />
-									<p>CATEGORY</p>
-								</Link>
-							</li>
-							{/* 팝업 및 배경을 div 내부에서 조건부 렌더링 */}
-							{isVisiable && (
-								<div
-									className={`absolute left-0 top-full pt-8 pb-10 font-normal bg-white w-screen z-20`}
-									style={{ left: `-${popupLeft}px` }}
-								>
-									<ul className="m-auto w-7xl flex justify-between gap-10">
-										{categoryMenuData.map((cat) => (
-											<li key={cat.title}>
-												<p className="font-bold mb-2">{cat.title}</p>
-												<ul className="flex flex-col gap-2">
-													{cat.items.map((item) => (
-														<li key={item.label}>
-															<Link
-																className="block w-full hover:underline"
-																href={item.href}
-															>
-																{item.label}
-																{item.new && (
-																	<span className="absolute -translate-y-1 ml-1 pointer-events-none text-xs text-red-500">
-																		New
-																	</span>
-																)}
-															</Link>
-														</li>
-													))}
-												</ul>
-											</li>
-										))}
-									</ul>
-								</div>
-							)}
-						</div>
-						{/* 나머지 카테고리 메뉴 */}
-						{mainCategoryNav.map((item) => (
-							<li
-								key={item.label}
-								className={`h-full flex items-center custom-expanded-weight`}
-							>
-								<Link className={item.className || ""} href={item.href}>
-									{item.label}
-								</Link>
-							</li>
-						))}
-					</ul>
-				</nav>
+                        <span
+                          id="shoppingList"
+                          className="block absolute bottom-0 right-0 bg-black min-w-[1.0625rem] rounded-full text-white text-xs text-center px-1"
+                        >
+                          0
+                        </span>
+                      </Link>
+                    ) : (
+                      <Link
+                        href={item.href!}
+                        className="relative hover:text-primary"
+                        title={item.title}
+                      >
+                        <item.icon />
+                      </Link>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          {/* Category */}
+          <nav className="w-full relative h-12">
+            <h3 className="sr-only">Category</h3>
+            <ul className="flex items-center gap-10 w-full h-full font-bold text-sm relative">
+              <div
+                className="relative h-full flex items-center"
+                onMouseEnter={() => setIsVisiable(true)}
+                onMouseLeave={() => setIsVisiable(false)}
+                ref={categoryRef}
+              >
+                <li className="h-full flex items-center custom-expanded-weight custom-before-division">
+                  <Link
+                    href="/"
+                    className="relative hover:text-primary flex items-center gap-3"
+                  >
+                    <HamburgerMenuIcon title="hamburger menu" className="w-[18px] h-[16px]" />
+                    <p>CATEGORY</p>
+                  </Link>
+                </li>
+                {/* 팝업 및 배경을 div 내부에서 조건부 렌더링 */}
+                {isVisiable && (
+                  <div
+                    className={`absolute left-0 top-full pt-8 pb-10 font-normal bg-white w-screen z-20`}
+                    style={{ left: `-${popupLeft}px` }}
+                  >
+                    <ul className="m-auto w-7xl flex justify-between gap-10">
+                      {categoryMenuData.map((cat) => (
+                        <li key={cat.title}>
+                          <p className="font-bold mb-2">{cat.title}</p>
+                          <ul className="flex flex-col gap-2">
+                            {cat.items.map((item) => (
+                              <li key={item.label}>
+                                <Link
+                                  className="block w-full hover:underline"
+                                  href={item.href}
+                                >
+                                  {item.label}
+                                  {item.new && (
+                                    <span className="absolute -translate-y-1 ml-1 pointer-events-none text-xs text-red-500">
+                                      New
+                                    </span>
+                                  )}
+                                </Link>
+                              </li>
+                            ))}
+                          </ul>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </div>
+              {/* 나머지 카테고리 메뉴 */}
+              {mainCategoryNav.map((item) => (
+                <li
+                  key={item.label}
+                  className={`h-full flex items-center custom-expanded-weight`}
+                >
+                  <Link className={item.className || ""} href={item.href}>
+                    {item.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </section>
 			</header>
 			{/* 카테고리 팝업 또는 검색창 활성화 시, 전체화면 배경 (모달 배경) */}
 			{(isVisiable || isSearchOpen) && (
@@ -337,35 +367,54 @@ const Header = () => {
 			)}
 			{/* 검색창 오버레이 */}
 			{isSearchOpen && (
-				<div className="fixed left-0 top-0 w-full pt-3 pb-4 z-50 bg-white bg-opacity-95"
+				<aside className="fixed left-0 top-0 w-full pt-3 pb-4 z-50 bg-white bg-opacity-95"
           style={{ height: `${HEADER_HEIGHT + CATEGORY_HEIGHT}px` }}
         >
-					<div className="relative overflow-hidden max-w-7xl mx-auto pr-3 flex gap-6 bg-search group">
-            <SearchIcon title="search icon" className="absolute left-2 top-1/2 -translate-y-1/2 w-[18px] h-[18px]
-            transition-all duration-100
-            group-focus-within:-left-4"/>
-						<input
-							type="text"
-							placeholder="Search by Brands, Product or Category"
-							className="w-full p-2 pl-8 text-sm
+					<section className="max-w-7xl m-auto bg-search">
+            <div className="relative overflow-hidden mb-6 pr-3 flex gap-6 group">
+              <SearchIcon title="search icon" className="absolute left-2 top-1/2 -translate-y-1/2 w-[18px] h-[18px] text-placeholder
               transition-all duration-100
-              focus:outline-none group-focus-within:pl-2
-              "
-							autoFocus
-							value={searchValue}
-							onChange={(e) => setSearchValue(e.target.value)}
-						/>
-            { searchValue !== "" && <button className="cursor-pointer" onClick={() => setSearchValue('')}>
-              <CloseIcon title="close" className="block rounded-full bg-gray-200 w-[18px] h-[18px]" />
-              </button> }
-					</div>
-          <button
-            className="cursor-pointer absolute top-4 right-4"
-            onClick={() => setIsSearchOpen(false)}
-          >
-            <CloseIcon title="close" className="w-[32px] h-[32px]" />
-          </button>
-				</div>
+              group-focus-within:-left-4"/>
+              <input type="text" placeholder="Search by Brands, Product or Category"
+                className="w-full p-2 pl-8 text-sm
+                          transition-all duration-100
+                          focus:outline-none group-focus-within:pl-2"
+                autoFocus
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
+              />
+              {/* Input에 텍스트 입력 시, input 값 제거하는 X 버튼 출력 */}
+              { searchValue !== "" && <button className="cursor-pointer" onClick={() => setSearchValue('')}>
+                <CloseIcon title="close" className="block rounded-full bg-gray-200 w-[18px] h-[18px]" />
+                </button> }
+            </div>
+            <button
+              className="absolute top-4 right-4 cursor-pointer"
+              onClick={() => setIsSearchOpen(false)}
+            >
+              <CloseIcon title="close" className="w-[36px] h-[36px]" />
+            </button>
+            {/* Search 목록 */}
+            <div className="flex gap-x-20">
+            {[
+              { title: "Most Searched Brands", list: SearchedData.SearchedBrands },
+              { title: "Popular", list: SearchedData.Popular },
+            ].map((section) => (
+              <Fragment key={section.title}>
+                <ol>
+                <h3 className="mb-5 font-bold">{section.title}</h3>
+                  {section.list.map((item, idx) => (
+                    <li key={idx} className="mb-3 cursor-pointer">
+                      <span className="inline-block min-w-[1.25rem] text-right mr-2 font-bold">{idx + 1}</span>
+                      {item}
+                    </li>
+                  ))}
+                </ol>
+              </Fragment>
+            ))}
+            </div>
+          </section>
+				</aside>
 			)}
 			<HeaderBannerText />
 		</>

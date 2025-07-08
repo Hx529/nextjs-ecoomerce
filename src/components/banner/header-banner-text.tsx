65216ce917f526888bc66b33a6926c3bf947f748
saan <hansan0529@gmail.com>
2025-07-08 14:45:22 +0900

    텍스트 데이터 정리, 프로모션 텍스트 클래스네임 상속

--- src/components/banner/header-banner-text.tsx
+++ src/components/banner/header-banner-text.tsx
@@ -4,50 +4,14 @@
 import { useEffect, useState } from "react";
 
 const banners = [
-{
-  content: (
-    <>
-      July Gift for All Members – Enjoy{'\u00A0'}<b>10% Off</b>{'\u00A0'}in Our App |{'\u00A0'}<b>2025JUL10</b>
-    </>
-  ),
-  // link: "/daily-checkin",
-  // icon: (
-  //   <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-  //       <path d="M5.25033 12.1663L9.91699 7.49967L5.25033 2.83301" stroke="#fff" strokeLinecap="square" />
-  //   </svg>
-  // )
-},
-{
-  content: (
-    <>
-      Start shopping on our APP and get{'\u00A0'}<b>15% OFF</b>{'\u00A0'}|{'\u00A0'}<b>WLCMAPP</b>
-    </>
-  ),
-},
-{
-  content: (
-    <>
-      <b>10% Off</b>{'\u00A0'}First Order｜7 Days Only｜Code:{'\u00A0'}<b>WLCM2MSS</b>
-    </>
-  )
-},
-{
-  content: (
-    <>
-      Official online store for leading Korean fashion brands
-    </>
-  )
-},
-{
-  content: (
-    <>
-      FREE SHIPPING on all orders $200+
-    </>
-  )
-}
+  { content: <>July Gift for All Members – Enjoy{' '}<b>10% Off</b>{' '}in Our App |{' '}<b>2025JUL10</b></> },
+  { content: <>Start shopping on our APP and get{' '}<b>15% OFF</b>{' '}|{' '}<b>WLCMAPP</b></> },
+  { content: <><b>10% Off</b>{' '}First Order｜7 Days Only｜Code:{' '}<b>WLCM2MSS</b></> },
+  { content: <>Official online store for leading Korean fashion brands</> },
+  { content: <>FREE SHIPPING on all orders $200+</> }
 ];
 
-export default function HeaderBannerText() {
+export default function HeaderBannerText({ className = ""}) {
   const [activeIndex, setActiveIndex] = useState(0); // i 활성화 텍스트
 
   // f 변경 카운트다운
@@ -59,7 +23,7 @@ export default function HeaderBannerText() {
   }, []);
 
   return (
-    <div className="relative grid w-full place-items-center min-h-10 py-2">
+    <div className={`relative grid w-full place-items-center min-h-10 py-2 bg-gray-100 ${className}`}>
       <span className="sr-only">Promotional text</span>
       {banners.map((banner, i) => (
         <div

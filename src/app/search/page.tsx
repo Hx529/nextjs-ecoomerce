5f95f3e81e8dbfd93c57d849bad90df806855a55
saan <hansan0529@gmail.com>
2025-07-08 14:46:12 +0900

    Search 페이지 카테고리 선택 추가

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,12 +1,41 @@
 'use client';
+import HeaderBannerText from "@/components/banner/header-banner-text";
 import useSearchKeywordSaver from "@/hooks/SearchKeywordSaver";
+import { useState } from "react";
+
+const SearchPageMetadata = [
+  "All",
+  "Tops",
+  "Overwear",
+  "Pants",
+  "Dresses & Skirts",
+  "Accessories",
+  "Bags",
+  "Shoes",
+  "Active",
+  "Underwear"
+]
 
 export default function SearchPage() {
   const keyword = useSearchKeywordSaver();
 
+  const [activeCategory, setActiveCategory] = useState("All");
+
   return (
     <>
-      <h2>Search results for &quot;{keyword}&quot;</h2>
+      <HeaderBannerText className="mb-13" />
+      <span className="sr-only">Search Page</span>
+      <section className="main-container">
+        <h2 className="mb-4 font-bold text-[1.75rem]">Search results for &apos;{keyword}&apos;</h2>
+        <div className="flex gap-2">
+          {SearchPageMetadata.map((item , i) => (
+            <button  key={i} className={`border rounded-full h-8 px-3 text-sm border-gray-300
+              ${activeCategory === item ? 'bg-black text-white' : ''}`}
+              onClick={() => setActiveCategory(item)}
+              >{item}</button>
+          ))}
+        </div>
+      </section>
     </>
   )
 }
